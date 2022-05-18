671343587d226601434eef7929914f9866468ca2
leyendanerd <leyendanerd@gmail.com>
2022-05-17 22:33:46 -0400

    yes

--- src/app.py
+++ src/app.py
@@ -8,11 +8,11 @@ data_empleados = [
      "picture": "https://www.baarty.com/articulos/wp-content/uploads/2013/04/motivando-empleado-bares.jpg"},
     {"nombre": "Carlos", "apellido": "Aguilar",
      "picture": "https://www.bbva.com/wp-content/uploads/2013/03/carlos-torres-vila-ceo-bbva-1024x885.jpg"},
-    {"nombre": "Ingrid", "apellido": "Guerrero", "picture": "https://www.clasibo.com/aviso_images/190626_041735.jpg"}
+    {"nombre": "Ingrid", "apellido": "Guerrero", "picture": "https://www.clasibo.com/aviso_images/190626_041735.jpg"},
     {"nombre": "Javier", "apellido": "Catalan",
-     "picture": "https://factorialhr.es/wp-content/uploads/2019/12/20130507/empleados-felices-portada.jpg"},
+     "picture": "https://factorialhr.es/wp-content/uploads/2019/12/20130507/empleados-felices-portada.jpg"}
     {"nombre": "Lhordes", "apellido": "Catln",
-     "picture": "https://static9.depositphotos.com/1594308/1163/i/950/depositphotos_11632583-stock-photo-successful-accountant.jpg"},
+     "picture": "https://static9.depositphotos.com/1594308/1163/i/950/depositphotos_11632583-stock-photo-successful-accountant.jpg"}
 ]
 
 # todo me permite crear una clave de seguridad para nuestra app
